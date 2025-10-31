fixed off by one error

cascadia/depthcharge/data/spectrum_datasets.py
@@ -140,14 +140,14 @@ class SpectrumDataset(Dataset):
             offsets.append(grp.attrs["n_spectra"])
             self._file_map[grp.attrs["path"]] = idx
 
-        self._file_offsets = np.cumsum([0] + offsets)
+        self._file_offsets = np.cumsum(offsets)
 
         # Build a map of 1D indices to 2D locations:
         grp_idx = 0
         for lin_idx in range(self._file_offsets[-1]):
             grp_idx += lin_idx >= self._file_offsets[grp_idx + 1]
             row_idx = lin_idx - self._file_offsets[grp_idx]
-            self._locs[lin_idx] = (grp_idx-1, row_idx)
+            self._locs[lin_idx] = (grp_idx, row_idx)
 
         self._offsets = None
         _ = self.offsets  # Reinitialize the offsets.